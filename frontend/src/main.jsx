a6157cbd56d7760844690ecec7558f295038f812
Anas Salem <anas@alMacBook-Air-alkhas-bAnas.local>
2025-09-08 09:38:31 +0300

    modified main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Eleganza-Home'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
